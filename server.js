fa368fffcd115a0cc9a894fdea78e763b96c624a
bhargab24 <32012786+bhargab24@users.noreply.github.com>
2018-02-14 09:25:27 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res){
+    res.send("Artcile one page will be served..");
+});
+
+app.get('/article-two', function(req, res){
+    res.send("Artcile two page will be served..");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
