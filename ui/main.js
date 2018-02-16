3de45e69d83f4b6d206eb7fd0c4e294f29bfe7d9
bhargab24 <32012786+bhargab24@users.noreply.github.com>
2018-02-16 05:25:17 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,6 @@
 var button = document.getElementById('counter');
 button.onclick = function(){
-    var request = XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.DONE)
-        {
-            if(request.status === 200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
+var counter = 12;
+                    var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET', 'http://bhargabchoudhury30.imad.hasura-app.io/counter', true);
-    request.send(null);
 };
